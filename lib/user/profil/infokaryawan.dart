77c9175085694d851f7f5de5ca1b10d1159904bf
adindaayuazzahra <4519210056@univpancasila.ac.id>
2024-03-13 15:37:05 +0700

    halaman info karyawan

--- lib/user/profil/infokaryawan.dart
+++ lib/user/profil/infokaryawan.dart
@@ -1,4 +1,6 @@
+import 'package:appjmtm/common/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Infokaryawan extends StatefulWidget {
   const Infokaryawan({Key? key}) : super(key: key);
@@ -10,6 +12,22 @@ class Infokaryawan extends StatefulWidget {
 class _InfokaryawanState extends State<Infokaryawan> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        elevation: 6,
+        centerTitle: true,
+        shadowColor: secondaryColor,
+        iconTheme: const IconThemeData(color: putih),
+        title: Text(
+          'Info Karyawan',
+          style: GoogleFonts.heebo(
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.7,
+            color: putih,
+          ),
+        ),
+      ),
+    );
   }
 }
